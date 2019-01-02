78cca8b0a8a5a405207a28b6cc1e3d348dcd8a28
Jakob Fix <jakob.fix@gmail.com>
2019-01-02 13:18:38 +0100

    Add @mereth's changes (4x faster!)

--- index.js
+++ index.js
@@ -6,20 +6,17 @@ const multistream = require('multistream')
 const ipcidr = require('ip-cidr')
 const R = require('ramda')
 
-
 const inputs = [
     fs.createReadStream('data/GeoLite2-Country-Blocks-IPv4.csv'),
     fs.createReadStream('data/GeoLite2-Country-Blocks-IPv6.csv')
 ]
 const geoLookup = fs.createReadStream('data/GeoLite2-Country-Locations-en.csv')
-const output = fs.createWriteStream('data/out.csv')
+const output = fs.createWriteStream('data/GeoIPCountryWhois.csv')
 const parser = parse({})
 const stringifier = stringify({quoted: true})
 
-const removeHeaders = transform((line, callback) => {
-    if (line[0] !== 'network') {
-        callback(null, line)
-    }
+const removeHeaders = transform((line) => {
+    if (line[0] !== 'network') return line
 })
 
 // take a CIDR range and return an array [startString, endString, startInt, endInt]
@@ -28,81 +25,76 @@ const convertCidrToRange = (cidr) => {
     const range = network.toRange()
     const start = network.start({type: 'bigInteger'}).toString()
     const end = network.end({type: 'bigInteger'}).toString()
-    return R.insertAll(2, [start, end], range)
+    return range.concat([start, end])
 }
 
 // transforms a incoming line by adding IP addresses and removes the CIDR range
-const resolveIps = transform((line, callback) => {
-    console.time('resolveIps')
-    callback(null, R.pipe(
-        R.insertAll(1, convertCidrToRange(line[0])),
-    )(line))
-}, {
-    parallel: 100
-}).on('error', (err) => console.log(err.message)
-).on('end', () => console.timeEnd('resolveIps'))
+const resolveIps = transform((line) => R.pipe(R.insertAll(1, convertCidrToRange(line[0])))(line))
 
 const getGeoInfo = (geoid) => {
-    const info = R.filter(R.propEq('id', geoid))(geoNames)
-    if (R.equals(info.length, 1)) {
-        const o = info[0]
-        return [o.iso, o.name]
+    const info = geoNames.get(geoid);
+    if (info) {
+        return [info.iso, info.name]
     }
     return ['ZZ', `Unknown ${geoid}`]
 }
 
-const resolveGeoNames = transform((line, callback) => {
+const resolveGeoNames = transform((line) => {
     const arr = getGeoInfo(line[5])
     const newLine = R.insertAll(5, arr)(line)
-    callback(null, newLine)
-}, {
-    parallel: 100
-}).on('error', (err) => console.log(err.message))
+    return newLine
+})
 
-const cleanUpCells = transform((line, callback) => {
+const cleanUpCells = transform((line) => {
     const newLine = R.pipe(
         R.remove(0, 1),
         R.remove(6, 5)
     )(line)
-    callback(null, newLine)
-}, {
-    parallel: 100
-}).on('error', (err) => console.log(err.message))
-
-const loadGeoNames = new Promise((resolve, reject) => {
-    console.time('loadGeoNames')
-    let names = []
-    const parser = parse({columns:true})
-    geoLookup.pipe(parser)
-    parser.on('readable', () => {
-        let record
-        while (record = parser.read()) {
-            names.push({
-                id: record.geoname_id, 
-                name: record.country_name, 
-                iso: record. country_iso_code
-            })
-        }
-    })
-    parser.on('error', (error) => reject(error))
-    parser.on('end', () => {
-        console.log(`INFO: loaded ${names.length} geonames.`)
-        console.timeEnd('loadGeoNames')
-        return resolve(names)
-    })
+    return newLine
 })
 
-let geoNames = [];
+let geoNames = null;
 
 (async () => {
-    geoNames = await loadGeoNames
+    geoNames = await new Promise((resolve, reject) => {
+        console.time('loadGeoNames')
+        console.log("> loadGeoNames")
+        let names = new Map()
+        const parser = parse({columns:true})
+        geoLookup.pipe(parser)
+        parser.on('readable', () => {
+            let record
+            while (record = parser.read()) {
+                names.set(record.geoname_id, {
+                    id: record.geoname_id, 
+                    name: record.country_name, 
+                    iso: record. country_iso_code
+                })
+            }
+        })
+        parser.on('error', (error) => reject(error))
+        parser.on('end', () => {
+            console.log(`INFO: loaded ${names.size} geonames.`)
+            console.timeEnd('loadGeoNames')
+            return resolve(names)
+        })
+    })
 
-    await multistream(inputs)
-        .pipe(parser)
-        .pipe(removeHeaders)
-        .pipe(resolveIps)
-        .pipe(resolveGeoNames)
-        .pipe(cleanUpCells)
-        .pipe(stringifier)
-        .pipe(output)
+    await new Promise((resolve, reject) => {
+        console.time('doTheWork')
+        console.log("> doTheWork")
+        
+        multistream(inputs)
+            .pipe(parser)
+            .pipe(removeHeaders)
+            .pipe(resolveIps)
+            .pipe(resolveGeoNames)
+            .pipe(cleanUpCells)
+            .pipe(stringifier)
+            .pipe(output)
+            .on('close', () => {
+                console.timeEnd('doTheWork')
+                resolve()
+            })
+    })
 })()
